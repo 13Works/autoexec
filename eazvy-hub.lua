51be08e3935f74c5c53e9d004b72fa37c69c0641
13Works <bonesisuseful.alt13@gmail.com>
2023-07-28 15:45:19 -0400

    Update eazvy-hub.lua

--- eazvy-hub.lua
+++ eazvy-hub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Eazvy/Eazvy-Hub/main/GameLoader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Eazvy/Eazvy-Hub/main/Games/920587237.lua"))()
